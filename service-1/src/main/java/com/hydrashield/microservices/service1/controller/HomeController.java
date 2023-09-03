d285151f063211e782e16f09cd2892577b74d7bf
Pethani Smit <86365190+HydraShield@users.noreply.github.com>
2023-09-03 17:27:09 +0530

    add hello api path

---
+++ service-1/src/main/java/com/hydrashield/microservices/service1/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.hydrashield.microservices.service1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @Value("${server.port}")
+    private String port;
+    @GetMapping("hello")
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.ok("Hello from service 1, running on port: "+port);
+    }
+}
